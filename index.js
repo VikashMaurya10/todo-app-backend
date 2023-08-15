6434f0bd8a61210061e0727d313e9e8fb2a451d9
VikashMaurya10 <VikashMaurya10stp@gmail.com>
2023-08-15 09:29:07 +0530

    sdjbh

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
